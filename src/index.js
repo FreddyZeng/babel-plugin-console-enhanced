b7b7e39f04dec5a3820bef1bd26e435d039f1a12
mcuking <mcuking.tang@gmail.com>
2019-04-24 17:44:48 +0800

    feat: 初步实现在 console 内容前加入文件名

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-var babel = require('babel-core');
-var t = require('babel-types');
+module.exports = babel => {
+  const { types: t } = babel;
+  const visitor = {
+    CallExpression(path, state) {
+      const { node } = path;
+      const { callee, arguments } = node;
+      if (
+        callee.type === 'MemberExpression' &&
+        callee.object.name === 'console'
+      ) {
+        const mark = `FileName - ${getLastWord(
+          state.file.opts.filename,
+          '/'
+        )}: `;
+        const stringNode = t.stringLiteral(mark);
+        arguments.unshift(stringNode);
+      }
+    }
+  };
 
-const visitors = {};
+  return {
+    name: 'babel-plugin-mark-console-position',
+    visitor
+  };
+};
 
-module.exports = babel => ({ visitors });
+function getLastWord(str, separator) {
+  return str.split(separator)[str.split(separator).length - 1];
+}
