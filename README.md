996a85ea11d4953403bbef3a66664d652f76ddab
mcuking <mcuking.tang@gmail.com>
2019-05-16 12:52:57 +0800

    feat: 完成主体功能，即向 console 内容自动添加必要信息

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
 # babel-plugin-console-enhanced
 
-A babel plugin that helps enrich the content of console expression.
+Babel plugin helps enhance the content of console expression.
+
+## Usage
+
+```bash
+npm install babel-plugin-console-enhanced -D
+```
+
+or
+
+```bash
+yarn add babel-plugin-console-enhanced -D
+```
+
+Via `.babelrc.js` or `babel.config.js` or babel-loader.
+
+```javascript
+{
+  "plugins": [["console-enhanced", options]]
+}
+```
+
+### options
+
+`options` need to be an object.
+
+```javascript
+{
+  "addFilename": false,
+  "addLine": true,
+  "addColumn": false,
+  "customContent": 'hello world'
+}
+```
+
+- `addFilename` &mdash; optional, default value is `false`, if add filename of the file contains console expression to content or not.
+
+- `addLine` &mdash; optional, default value is `false`, if add line of console expression to content or not.
+
+- `addColumn` &mdash; optional, default value is `false`, if add column of console expression to content or not.
+
+- `customContent` &mdash; optional, default value is `""`, if not a null string, it will be added to the content of console expression.
+
+## Example
+
+### From
+
+```javascript
+console.log(a);
+```
+
+### To
+
+```javascript
+console.log('filename cool.js , line 3, column 0, hello world, ', a);
+```
+
+## Notes
+
+If you want to use this plugin via `.babelrc`, it should be configured in `.babelrc.js`.
+And `.babelrc` should be like this:
+
+```javascript
+{
+  "presets": ["./.babelrc.js"]
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
 module.exports = babel => {
   const { types: t } = babel;
   const visitor = {
-    CallExpression(path, state) {
-      const { node } = path;
-      const { callee, arguments } = node;
+    CallExpression(path) {
       if (
-        callee.type === 'MemberExpression' &&
-        callee.object.name === 'console'
+        path.node.callee.type === 'MemberExpression' &&
+        path.node.callee.object.name === 'console'
       ) {
-        const mark = `FileName - ${getLastWord(
-          state.file.opts.filename,
+        if (this.opts && !isObject(this.opts)) {
+          path.stop();
+          return console.error(
+            '[babel-plugin-console-enhanced]: options need to be an object.'
+          );
+        }
+
+        const filename = `filename ${getLastWord(
+          this.file.opts.filename,
           '/'
-        )}: `;
-        const stringNode = t.stringLiteral(mark);
-        arguments.unshift(stringNode);
+        )} `;
+        const line = `line ${path.node.loc.start.line}`;
+        const column = `column ${path.node.loc.start.column}`;
+
+        let description = '';
+
+        for (const key in this.opts) {
+          if (this.opts.hasOwnProperty(key)) {
+            const val = this.opts[key];
+            switch (key) {
+              case 'addFilename':
+                description = val ? `${description}${filename}, ` : description;
+                break;
+              case 'addLine':
+                description = val ? `${description}${line}, ` : description;
+                break;
+              case 'addColumn':
+                description = val ? `${description}${column}, ` : description;
+                break;
+              case 'customContent':
+                description = val
+                  ? `${description}${this.opts.customContent}, `
+                  : description;
+                break;
+              default:
+                break;
+            }
+          }
+        }
+
+        if (description) {
+          path.node.arguments.unshift(t.stringLiteral(description));
+        }
       }
     }
   };
@@ -26,4 +61,8 @@ module.exports = babel => {
 
 function getLastWord(str, separator) {
   return str.split(separator)[str.split(separator).length - 1];
+}
+
+function isObject(obj) {
+  return Object.prototype.toString.call(obj) === '[object Object]';
 }
